30f39f170a6ca14bd599fa8e3b2b5a9704aff99c
nsds42 <nicolas.doutres@gmail.com>
2018-02-04 00:56:36 +0100

    Modifications

--- webpack.config.js
+++ webpack.config.js
@@ -14,18 +14,6 @@ Encore
          resolveUrlLoader: false
      })
      .autoProvidejQuery()
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
-
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
